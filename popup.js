9ce41578a7e11ac17f5086aed957f468d710583a
Jeanne <jpascua93@gmail.com>
2018-05-24 17:37:03 -0700

    Trying to get popup.js to run on each page load

--- popup.js
+++ popup.js
@@ -1,11 +1,18 @@
 let submitButton = document.getElementById("submit");
 
-submitButton.onclick = function(element) {
+submitButton.onclick = function() {
     chrome.storage.sync.set(
         {textInput1: document.getElementById("field1").value,
         textInput2: document.getElementById("field2").value});
 
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.executeScript(tabs[0].id, {file: "replace.js"})
+        chrome.tabs.executeScript(tabs[0].id, {file: "replace.js"});
     });
 };
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    console.log("onUpdated Listener!");
+    if(changeInfo.status == "complete") {
+        chrome.tabs.executeScript(tabId, {file: "replace.js"});
+    }
+});
